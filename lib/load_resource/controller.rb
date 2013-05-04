36b7804281c9e8efee9a4d4fdc7cd04191bc1f92
Thiago Pinto <tapgyn@gmail.com>
2013-05-04 18:04:12 -0400

    version 1.0.3, accepts load_resource as a class method

--- lib/load_resource/controller.rb
+++ lib/load_resource/controller.rb
@@ -1,8 +1,22 @@
 module LoadResource
   module Controller
-    
+    extend ActiveSupport::Concern
+
     def load_resource(&block)
       Command.new(self).instance_eval(&block)
+    end
+
+    def filter_load_resource
+      block = self.class.load_resource_block
+      load_resource(&block)
+    end
+
+    module ClassMethods
+      attr_reader :load_resource_block
+      def load_resource(&block)
+        @load_resource_block = block
+        before_filter :filter_load_resource
+      end
     end
 
   end

0b7724d0daf4db4184d36f78a66b36e38febab3d
Thiago Pinto <tapgyn@gmail.com>
2014-06-30 21:43:40 -0400

    updated suggested controller template

--- lib/generators/before_actions/templates/controller.rb
+++ lib/generators/before_actions/templates/controller.rb
@@ -16,54 +16,82 @@ class <%= controller_class_name %>Controller < ApplicationController
   end
 
   # GET <%= route_url %>
+  # GET <%= route_url %>.json
   def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: <%= "@#{plural_table_name}" %> }
+    end
   end
 
-  # GET <%= route_url %>/1
-  def show
-  end
+
 
   # GET <%= route_url %>/new
   def new
+  end
+
+  # GET <%= route_url %>/1
+  # GET <%= route_url %>/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: <%= "@#{singular_table_name}" %> }
+    end
   end
 
   # GET <%= route_url %>/1/edit
   def edit
   end
 
+
+
   # POST <%= route_url %>
+  # POST <%= route_url %>.json
   def create
-    if @<%= orm_instance.save %>
-      redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully created.'" %>
-    else
-      render action: 'new'
+    respond_to do |format|
+      if @<%= orm_instance.save %>
+        format.html { redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully created.'" %> }
+        format.json { render json: <%= "@#{singular_table_name}" %>, status: :created }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: <%= "@#{orm_instance.errors}" %>, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT <%= route_url %>/1
+  # PATCH/PUT <%= route_url %>/1.json
   def update
-    if @<%= orm_instance.update("#{singular_table_name}_params") %>
-      redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully updated.'" %>
-    else
-      render action: 'edit'
+    respond_to do |format|
+      if @<%= orm_instance.update("#{singular_table_name}_params") %>
+        format.html { redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully updated.'" %> }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: <%= "@#{orm_instance.errors}" %>, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE <%= route_url %>/1
+  # DELETE <%= route_url %>/1
   def destroy
-    @<%= orm_instance.destroy %>
-    redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} was successfully destroyed.'" %>
+    respond_to do |format|
+      if @<%= orm_instance.destroy %>
+        format.html { redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} was successfully destroyed.'" %> }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: <%= "@#{orm_instance.errors}" %>, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
     # Only allow a trusted parameter "white list" through.
     def <%= "#{singular_table_name}_params" %>
-      <%- if attributes_names.empty? -%>
-      params[<%= ":#{singular_table_name}" %>]
-      <%- else -%>
       params.require(<%= ":#{singular_table_name}" %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
-      <%- end -%>
     end
 end
 <% end -%>

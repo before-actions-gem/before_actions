4fdd950130da58afa68a2a03b2ffbe8ce871415f
Thiago Pinto <tapgyn@gmail.com>
2014-09-12 17:38:24 -0300

    generator should be called before_actions:template, since this is all it really does

--- lib/generators/before_actions/template_generator.rb
+++ lib/generators/before_actions/template_generator.rb
@@ -1,6 +1,6 @@
 module BeforeActions
   module Generators
-    class InstallGenerator < Rails::Generators::Base
+    class TemplateGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
 
       def generate_template
